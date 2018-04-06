7969aabaa817367139f43908919626182cf26f21
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-04-05 21:13:41 -0300

    [UPDATE] Added some functions to hash class

--- hashing.cpp
+++ hashing.cpp
@@ -3,9 +3,59 @@
 //
 
 #include "hashing.h"
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
 class hashing{
+private:
+    string arr[];
+    int size;
+    int capacity;
+public:
+    hashing(int capacit){
+        arr = new string[capacit];
+        for(int i =0;i<capacit;i++){
+            arr[i] = NULL;
+        }
+        size = 0;
+        capacity = capacit;
+    }
+
+    int hash(std::string x){
+        if(size == capacity){
+            exceptionArrayFull(x);
+        }
+        else {
+            int i = hashFunc(x);
+            if (arr[i] != NULL) {
+                handleCollision();
+            } else {
+                arr[i] = x;
+            }
+        }
+    }
+
+    
+    void handleCollision(){
+
+    }
 
 
+    int hashFunc(std::string x){
+        return (x.length() * x.capacity() * random())%capacity;
+    }
+
+    int random(){
+        int result = rand();
+        return result;
+    }
+
+    //Pseudo exceptions
+    std::string exceptionArrayFull(std::string x){
+        std::cout<<"Array is full" << std::endl;
+        return x;
+    }
 
 };
