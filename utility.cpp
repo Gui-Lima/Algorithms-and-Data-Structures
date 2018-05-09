c4b06f3439b7c043640ad12352b24c249fcc743b
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-05-09 18:34:14 -0300

    Utility class w useful methods

---
+++ utility.cpp
@@ -0,0 +1,17 @@
+//
+// Created by guila on 09/05/18.
+//
+
+#include "utility.h"
+
+void utility::swap(long &a, long &b) {
+    long aux = a;
+    a = b;
+    b = aux;
+}
+
+long utility::max(long a, long b) {
+    if(a>=b){ return a;}
+    if(b>a){return b;}
+    return 0;
+}
---
+++ utility.h
@@ -0,0 +1,17 @@
+//
+// Created by guila on 09/05/18.
+//
+
+#ifndef ALGORITHMS_AND_DATA_STRUCTURES_UTILITY_H
+#define ALGORITHMS_AND_DATA_STRUCTURES_UTILITY_H
+
+
+class utility {
+public:
+    void swap(long &a, long &b);
+
+    long max(long a, long b);
+};
+
+
+#endif //ALGORITHMS_AND_DATA_STRUCTURES_UTILITY_H
