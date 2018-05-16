1b8f50af8ea5a28ef58c06c466bcf77bd920b5aa
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-05-16 11:22:34 -0300

    Modifying project to new classes

--- coinChange.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by guila on 16/05/18.
-//
-
-#include "coinChange.h"
--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
 #include "iostream"
 //write here what files fo include
-#include "Data-Structures/graph.h"
+#include "Dynamic-Programming/coinChange.h"
 
 
 using namespace std;
 
+
 int main() {
-    graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(2, 3);
-    g.addEdge(1,2);
-    g.print();
     return 0;
 }
 
