1bf41b9a32aa81d6fd846d8f7adfb725524ebde9
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-05-22 20:38:01 -0300

    Created backtrack folder w Queens problem

---
+++ BackTracking/nQueens.cpp
@@ -0,0 +1,5 @@
+//
+// Created by guila on 22/05/18.
+//
+
+#include "nQueens.h"
---
+++ BackTracking/nQueens.h
@@ -0,0 +1,14 @@
+//
+// Created by guila on 22/05/18.
+//
+
+#ifndef ALGORITHMS_AND_DATA_STRUCTURES_NQUEENS_H
+#define ALGORITHMS_AND_DATA_STRUCTURES_NQUEENS_H
+
+
+class nQueens {
+
+};
+
+
+#endif //ALGORITHMS_AND_DATA_STRUCTURES_NQUEENS_H
