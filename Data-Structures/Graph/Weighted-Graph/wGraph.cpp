d9d6455646d923b47680020dda41f1ab72843661
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-05-30 22:28:15 -0300

    Extracted Dijkstra from the graph class

--- Data-Structures/Graph/Weighted-Graph/wGraph.cpp
+++ Data-Structures/Graph/Weighted-Graph/wGraph.cpp
@@ -1,160 +1,68 @@
 
+
+#include <list>
 #include "wGraph.h"
 #include "iostream"
 #define INT_MAX 100000
 
 
-wGraph::wGraph(long tam, long nvert){
-    nVert = nvert;
+wGraph::wGraph(long tam){
     size = tam;
-    nVertAtual = 0;
-    pesoTotal=0;
-    h = new minHeapStructure(nVert);
-    d = new long[size];
-    e = new edge[nVert];
     g = new long*[size];
-    te = new vector<long>[size];
     for(long i =0;i<size;i++){
         g[i] = new long[size];
+        for(long j =0;j<size;j++){
+            g[i][j] = 0;
+        }
     }
 }
 
 
 void wGraph::addNode(long i, long j, long weight){
-    if(weight == 0){
+    if(i >= 0 && j>=0 && i<size && j<size){
         g[i][j] = weight;
         g[j][i] = weight;
-        te[i].push_back(j);
-        te[j].push_back(i);
-    }
-    else if(g[i][j] > weight || g[i][j] == 0){
-        g[i][j] = weight;
-        g[j][i] = weight;
-        te[i].push_back(j);
-        te[j].push_back(i);
-    }
-
-    pesoTotal += weight;
-
-    e[nVertAtual].dest = j;
-    e[nVertAtual].src = i;
-    e[nVertAtual].weight = weight;
-    nVertAtual++;
-
-}
-
-
-void wGraph::print(){
-    for(long i =0;i<size;i++){
-        std::cout<< i << " esta conectado com :";
-        for(long j=0;j<size;j++){
-            if(g[i][j] < INT_MAX && g[i][j] > 0){
-                std::cout << j << " com peso ( " << g[i][j] << " ) ";
-            }
-        }
-        std::cout << std::endl;
     }
 }
-
-long* wGraph::djkistra(long iNode) {
-    bool v[size];
-    for(long i =0;i<size;i++){
-        d[i] = INT_MAX;
-        v[i] = false;
-    }
-    //starting from initialNode
-    d[iNode] = 0;
-    h->insert(0, iNode);
-    for(long i = 0;i<size-1;i++){
-
-        long u = minDistHeap(v);
-        if(u == -1){
-           return d;
-        }
-        v[u] = true;
-
-        for(long j =0;j<te[u].size();j++){
-            long adj = te[u][j];
-            long adjW = g[u][adj];
-            if(!v[adj] && d[u] + adjW < d[adj]){
-                d[adj] = d[u] + adjW;
-                h->insert(d[adj], adj);
-            }
-        }
-
-    }
-
-    for(long i=0;i<size;i++){
-        std::cout << "minimal distance from " << iNode << " to " << i << " is " << d[i] << std::endl;
-    }
-
-    return d;
-}
-long* wGraph::newdjkistra(long iNode, long final) {
-    bool v[size];
-    vector <long> Q;
-    for(long i =0;i<size;i++){
-        d[i] = INT_MAX;
-        v[i] = false;
-        Q.push_back(i);
-    }
-    //starting from initialNode
-    d[iNode] = 0;
-    h->insert(0, iNode);
-    for(long i = 0;i<size-1;i++){
-        long u = minDistHeap(v);
-        if(u == -1){
-            return d;
-        }
-        v[u] = true;
-
-        for(long j =0;j<te[u].size();j++){
-            long adj = te[u][j];
-            long adjW = g[u][adj];
-            if(!v[adj] && d[u] + adjW < d[adj]){
-                d[adj] = d[u] + adjW;
-                h->insert(d[adj], adj);
-            }
-        }
-        if(u==final){
-            return d;
-        }
-    }
-    return d;
-}
-
-long wGraph::minDist(long *d, bool *v) {
-    long min = INT_MAX;
-    long result;
-    for(long i =0;i<size;i++){
-        if(d[i] < min && !v[i]){
-            min = d[i];
-            result = i;
-        }
-    }
-    return result;
-}
-
-
 
 long wGraph::getSize() {
     return size;
 }
 
-long wGraph::getNVert() {
-    return nVert;
-}
-
-long wGraph::minDistHeap(bool *v) {
-
-    long min = h->pop();
-    while(v[min]){
-        min = h->pop();
+void wGraph::bfs(long startingNode) {
+    bool visited[size];
+    for(int i =0;i<size;i++){
+        visited[i] = false;
     }
 
-    return min;
+    list <long> nodesQueue;
+    nodesQueue.push_front(startingNode);
+
+    while(!nodesQueue.empty()){
+        long node = nodesQueue.front();
+        cout << node << endl;
+        visited[node] = 1;
+        nodesQueue.pop_front();
+        for(int i =0;i<size;i++){
+            if(g[node][i] > 0 && !visited[i]){
+                nodesQueue.push_back(i);
+            }
+        }
+    }
 }
 
-long wGraph::soma() {
-    return pesoTotal;
+void wGraph::dfs(long startingNode) {
+
+}
+
+void wGraph::print() {
+    for(long i =0;i<size;i++){
+        std::cout<< i << " is connected with :";
+        for(long j=0;j<size;j++){
+            if(g[i][j] < INT_MAX && g[i][j] > 0){
+                std::cout << j << " with weight ( " << g[i][j] << " ) ";
+            }
+        }
+        std::cout << std::endl;
+    }
 }
--- Data-Structures/Graph/Weighted-Graph/wGraph.h
+++ Data-Structures/Graph/Weighted-Graph/wGraph.h
@@ -7,44 +7,25 @@
 #define INT_MAX 100000
 #include "../../Heap/Modified/minHeapStructure.h"
 #include "vector"
+
 class wGraph {
 private:
     long size;
-    long nVert;
-    long nVertAtual;
     long ** g;
-    long * d;
-    long pesoTotal;
-    std::vector<long> * te;
-    minHeapStructure *h;
-public:
 
-    struct edge{
-        long src, dest, weight;
-    };
+public:
 
     long getSize();
 
-    long getNVert();
-
-    edge* getEdges(){
-        return e;
-    }
-    long soma();
-
-    wGraph(long tam=0, long nvert = 0);
+    explicit wGraph(long tam=0);
 
     void addNode(long i, long j, long weight);
 
     void print();
-    long * newdjkistra(long iNode=0, long final=0);
-    long * djkistra(long iNode=0);
-    //djkistra aux
-    long minDist(long * d, bool* v);
 
-    long minDistHeap(bool * v);
-private:
-    edge* e;
+    void bfs(long startingNode=0);
+
+    void dfs(long startingNode=0);
 };
 
 
