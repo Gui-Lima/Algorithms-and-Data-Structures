6b8695b54b47409ffa638ec078bf9725a946e365
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-06-01 13:20:14 -0300

    Prim class working

--- Greedy-Algorithms/Minimum-Spamming-Tree/Prim/prim.cpp
+++ Greedy-Algorithms/Minimum-Spamming-Tree/Prim/prim.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "prim.h"
+#define INFINITE 30000
+prim::prim(wGraph g, long nVert) {
+    this->g = g;
+    mh = new minHeapStructure(nVert);
+    result = new wGraph(g.getSize());
+}
+
+void prim::mst() {
+    long keys[g.getSize()];
+    bool visited[g.getSize()];
+    long parent[g.getSize()];
+    for(int i =0;i<g.getSize();i++){
+        keys[i] = INFINITE;
+        visited[i] = false;
+    }
+
+    keys[0] = 0;
+    parent[0] = -1;
+    mh->insert(0,0);
+    for(int i =0;i<g.getSize();i++){
+        long u = minInKeysNotVisited(visited);
+        visited[u] = true;
+
+        result->addNode(u,parent[u],keys[u]);
+
+        for(int j =0;j<g.getSize();j++){
+            if(g.getGraph()[u][j] != 0 && !visited[j] && g.getGraph()[u][j] < keys[j]){
+                keys[j] = g.getGraph()[u][j];
+                mh->insert(keys[j], j);
+                parent[j] = u;
+            }
+        }
+    }
+
+    result->print();
+}
+
+long prim::minInKeysNotVisited(bool* visited ) {
+    long min = mh->pop();
+    while(visited[min]){
+        min = mh->pop();
+    }
+    return min;
+}
--- Greedy-Algorithms/Minimum-Spamming-Tree/Prim/prim.h
+++ Greedy-Algorithms/Minimum-Spamming-Tree/Prim/prim.h
@@ -5,9 +5,21 @@
 #ifndef ALGORITHMS_AND_DATA_STRUCTURES_PRIM_H
 #define ALGORITHMS_AND_DATA_STRUCTURES_PRIM_H
 
+#include "../../../Data-Structures/Graph/Weighted-Graph/wGraph.h"
+#include "../../../Data-Structures/DisjointSet/disjointSet.h"
 
 class prim {
+private:
+    wGraph g;
+    disjointSet d;
+    wGraph* result;
+    minHeapStructure* mh;
+public:
+    prim(wGraph g, long numberOfVertices);
 
+    void mst();
+
+    long minInKeysNotVisited(bool* visited);
 };
 
 
