50caca4b687f1e0e3f4dd2f41c698d2aa564b502
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-06-06 15:40:23 -0300

    Vertex Cover class w approximation algorithm

--- Approximation-Algorithms/Vertex Cover/vertexCover.cpp
+++ Approximation-Algorithms/Vertex Cover/vertexCover.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "vertexCover.h"
+#include "iostream"
+#include "vector"
+
+vertexCover::vertexCover(graph g, int numberOfVertices) {
+    this->toCheckGraph = g;
+    result = new bool[g.size()];
+}
+
+bool* vertexCover::randomEdgeSolution() {
+    int graphSize = toCheckGraph.size();
+    bool visited[graphSize];
+    for(int i =0;i<graphSize;i++){
+        visited[i] = false;
+    }
+
+    for(int i =0;i<graphSize;i++){
+        if(!visited[i]){
+            for(int j =0;j<graphSize;j++){
+                if(toCheckGraph.has(i, j) && !visited[j]){
+                    visited[i] = true;
+                    visited[j] = true;
+                    j = graphSize;
+                }
+            }
+        }
+    }
+
+    result=visited;
+    for(int i =0;i<graphSize;i++){
+        if(result[i]){
+            std::cout << i << " ";
+        }
+    }
+    return result;
+}
--- Approximation-Algorithms/Vertex Cover/vertexCover.h
+++ Approximation-Algorithms/Vertex Cover/vertexCover.h
@@ -5,8 +5,18 @@
 #ifndef ALGORITHMS_AND_DATA_STRUCTURES_VERTEXCOVER_H
 #define ALGORITHMS_AND_DATA_STRUCTURES_VERTEXCOVER_H
 
+#include "../../Data-Structures/Graph/Non-Weight-Graph/graph.h"
+#include "vector"
+
 
 class vertexCover {
+private:
+    graph toCheckGraph;
+    bool* result;
+public:
+    vertexCover(graph g, int numberOfVertices);
+
+    bool* randomEdgeSolution();
 
 };
 
